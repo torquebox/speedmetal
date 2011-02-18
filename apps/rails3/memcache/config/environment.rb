c0def591d9f85b620c00641a823fa624a0986e32
Ben Browning <bbrownin@redhat.com>
2011-02-18 13:24:34 -0500

    memcache test app

---
+++ apps/rails3/memcache/config/environment.rb
@@ -0,0 +1,13 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Memcache::Application.initialize!
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # Only works with DalliStore
+    Rails.cache.reset if forked
+  end
+end
+
---
+++ apps/rails3/memcache/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memcache::Application.config.secret_token = '5db81d4da4c82784168f41c1020146b8c2b15af4d589c5b1bc8a1ac6735ebca4e0e2a943bd3c891f738257c41fa70e0eb3d2d388de86335b69a93193ae0419bf'
