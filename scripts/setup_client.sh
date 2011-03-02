744725983dca3f88afd03276d576a600d4da9eda
Ben Browning <bbrownin@redhat.com>
2011-03-02 08:16:38 -0500

    Add shell scripts to ease setting up client and server instances

---
+++ scripts/setup_client.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# exit if an error occurs in any simple command
+set -e
+# exit if an error occurs in a pipeline
+set -o pipefail
+
+usage() {
+    echo "Usage: `basename $0` server_instance_ip_address"
+    echo "       server_instance_ip_address is the IP address of the server instance"
+    exit 1
+}
+
+[[ $# -ne 1 ]] && usage
+
+SERVER_IP=$1
+
+cd ~
+
+# Install necessary RPMs
+sudo yum install -y erlang wget make git perl-Template-Toolkit gnuplot
+
+# Clone SpeedMetal
+git clone git://github.com/torquebox/speedmetal.git
+
+# Install Tsung
+wget http://tsung.erlang-projects.org/dist/tsung-1.3.3.tar.gz
+tar xf tsung-1.3.3.tar.gz
+cd tsung-1.3.3
+./configure
+make
+sudo make install
+cd ../
+
+# Add /etc/hosts entry for our server instance
+echo "$SERVER_IP server" >> sudo tee -a /etc/hosts
---
+++ scripts/setup_server.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# exit if an error occurs in any simple command
+set -e
+# exit if an error occurs in a pipeline
+set -o pipefail
+
+usage() {
+    echo "Usage: `basename $0` server_type rds_instance_ip_address"
+    echo "       server_type is one of torquebox, trinidad, unicorn, passenger, or thin"
+    echo "       rds_instance_ip_address is the IP address of the Amazon RDS instance"
+    exit 1
+}
+
+[[ $# -ne 2 ]] && usage
+
+SERVER_TYPE=$1
+RDS_IP=$2
+
+# Create writable directory under /mnt because the root
+# partition isn't large on EC2 instances
+sudo mkdir /mnt/data
+sudo chmod 777 /mnt/data
+cd /mnt/data
+
+# Install necessary RPMs
+sudo yum install -y erlang git
+
+# Clone SpeedMetal
+git clone git://github.com/torquebox/speedmetal.git
+
+# Add /etc/hosts entry for our RDS instance
+echo "$RDS_IP database" >> sudo tee -a /etc/hosts
+
+case "$SERVER_TYPE" in
+    torquebox)
+        # Install necessary RPMs
+        sudo yum install -y java-1.6.0-openjdk wget unzip
+        # Install latest TorqueBox dev build
+        wget http://torquebox.org/torquebox-dev.zip
+        unzip torquebox-dev.zip
+        ln -s torquebox-1.0.0.CR1-SNAPSHOT/ torquebox-current
+        echo "export TORQUEBOX_HOME=/mnt/data/torquebox-current" >> ~/.bash_profile
+        echo "export JBOSS_HOME=\$TORQUEBOX_HOME/jboss" >> ~/.bash_profile
+        echo "export JRUBY_HOME=\$TORQUEBOX_HOME/jruby" >> ~/.bash_profile
+        echo "export PATH=\$JRUBY_HOME/bin:\$PATH" >> ~/.bash_profile
+        source ~/.bash_profile
+        jruby -S gem install jruby-openssl
+        # Increase open file limit
+        echo "ec2-user hard nofile 4096" >> sudo tee -a /etc/security/limits.conf
+        echo "ec2-user shoft nofile 4096" >> sudo tee -a /etc/security/limits.conf
+        sudo ulimit -n 4096
+        ;;
+    trinidad)
+        # Install necessary RPMs
+        sudo yum install -y java-1.6.0-openjdk wget
+        # Install JRuby
+        wget http://jruby.org.s3.amazonaws.com/downloads/1.5.6/jruby-bin-1.5.6.tar.gz
+        tar xf jruby-bin-1.5.6.tar.gz
+        echo "export JRUBY_HOME=/mnt/data/jruby-1.5.6" >> ~/.bash_profile
+        echo "export PATH=\$JRUBY_HOME/bin:\$PATH" >> ~/.bash_profile
+        source ~/.bash_profile
+        jruby -S gem install jruby-openssl
+        # Install Trinidad
+        jruby -S gem install trinidad
+        ;;
+    passenger)
+        # Install necessary RPMs
+        sudo yum install -y ruby ruby-devel rubygems make gcc gcc-c++ \
+            curl-devel openssl-devel zlib-devel
+        # Install passenger
+        sudo gem install passenger
+        ;;
+    unicorn)
+        # Install necessary RPMs
+        sudo yum install -y ruby ruby-devel rubygems make gcc gcc-c++ \
+            curl-devel openssl-devel zlib-devel
+        # Install unicorn
+        sudo gem install unicorn
+        ;;
+esac
