545be942fe430be0fa0b7fc1d1d576c18c8e0b3e
Ben Browning <bbrownin@redhat.com>
2011-10-06 09:29:16 -0400

    Updated Spree graphs

--- results/rails3/spree/graphs/compare_ree_19.r
+++ results/rails3/spree/graphs/compare_ree_19.r
@@ -7,16 +7,16 @@ arg0 <- sub("--file=(.*)", "\\1", grep("--file=", commandArgs(), value = TRUE))
 dir0 <- dirname(arg0)
 source(file.path(dir0, "common.r"))
 
-opt = parse_params("compare_top")
+opt = parse_params("compare_ree_19")
 
-input_dirs <- c("torquebox-2.x/20110930-15:21/data",
-                "trinidad/20111005-16:59/data",
+input_dirs <- c("passenger_ree/20110930-15:41/data",
                 "passenger_19/20110930-17:32/data",
+                "unicorn_ree/20111003-12:40/data",
                 "unicorn_19/20111003-12:40/data")
 
-server_tags <- c("TorqueBox 2.x",
-                 "Trinidad",
+server_tags <- c("Passenger w/ REE",
                  "Passenger w/ 1.9.2",
+                 "Unicorn w/ REE",
                  "Unicorn w/ 1.9.2")
 
 draw_graph(list(dirs=input_dirs, tags=server_tags), opt)
